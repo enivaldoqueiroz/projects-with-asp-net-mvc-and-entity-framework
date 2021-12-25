7b366e5c8248adfecdaa06b8e5c9f1e6658a809e
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-12-25 13:20:36 -0300

    Chapter01 - Implementation of the Institution Controller and the Institution Model

---
+++ SolutionChapter01/Chapter01/Controllers/InstitutionController.cs
@@ -0,0 +1,30 @@
+﻿using Chapter01.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chapter01.Controllers
+{
+    public class InstitutionController : Controller
+    {
+        private static IList<Institution> institutions =
+            new List<Institution>
+            {
+                new Institution()
+                {
+                    InstitutionID = 1,
+                    Nome = "UniParaná",
+                    Endereco = "Paraná"
+                    
+                }
+            };
+
+        // Definition of an action called Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SolutionChapter01/Chapter01/Models/Institution.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chapter01.Models
+{
+    public class Institution
+    {
+        public long? InstitutionID { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+    }
+}
